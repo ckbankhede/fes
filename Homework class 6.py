50df689735cfb0f11292fd145031fb5050691469
ckbankhede <60286210+ckbankhede@users.noreply.github.com>
2026-06-19 15:17:05 -0500

    Add files via upload

---
+++ Homework class 6.py
@@ -0,0 +1,5 @@
+A= (input("Enter anything to check if it is a alphabet or not:"))
+if A.isalpha():
+    print(f"'{A}' is an alphabet letter / contains only alphabets.")
+else:
+    print(f"'{A}' is NOT an alphabet (it contains numbers, spaces, or symbols).")
