f2267a32d16f47e57e6b10c2d46058252ac809bc
ckbankhede <60286210+ckbankhede@users.noreply.github.com>
2026-07-05 14:30:47 -0500

    Add files via upload

---
+++ Homework 9.py
@@ -0,0 +1,6 @@
+age_of_user= int(input("Enter Your age to enter the Playground:"))
+if age_of_user>=10 and age_of_user<=20:
+    print("You are allowed to enter.")
+
+else:
+    print("Sorry! You are not allowed.")
