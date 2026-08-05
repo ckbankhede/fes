f5acc6a8318e45fb25deb0f729cf71489b7e0937
ckbankhede <60286210+ckbankhede@users.noreply.github.com>
2026-08-05 14:16:46 -0500

    Add files via upload

---
+++ Homework 15.py
@@ -0,0 +1,5 @@
+def resturant_bill(amount_of_food_ordered,price_of_food):
+    bill=amount_of_food_ordered*price_of_food
+    return bill
+    """This function generates the bill for the customer to pay for the resturant food ordered."""
+print(resturant_bill(19,12))
